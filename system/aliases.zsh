6b5bdd72b2b4aa8e4de53dc8c687612890d18771
chrisg <christopher.gateley@gmail.com>
2012-02-21 00:57:22 -0600

    Initial commit

---
+++ system/aliases.zsh
@@ -0,0 +1,13 @@
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -la'
+alias ll='ls -l'
+alias sl=ls # common typo
+
