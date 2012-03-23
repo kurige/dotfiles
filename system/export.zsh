25cca23cd944519febe8f1e9d85e4459c0d766db
Christopher Gateley <christopher.gateley@gmail.com>
2012-03-22 22:03:18 -0500

    Add 'jade' (javascript template language) vim mode submodule Rename system/path.zsh to system/export.zsh Add EC2 exports to export.zsh

---
+++ system/export.zsh
@@ -0,0 +1,8 @@
+
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.5/jars"
+
+export PATH="./node_modules/.bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
