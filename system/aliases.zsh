54cd7193fa63d8de71b26f3a0e15b5c956f79980
Christopher Gateley <christopher.gateley@gmail.com>
2012-07-17 13:14:31 -0400

    Useful 'bashagent' alias for creating bash session with ssh agents attached

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -14,8 +14,12 @@ alias sl=ls # common typo
 # Handy 'sha1' alias
 alias sha1='openssl dgst -sha1'
 
+# Create bash session with public keys attached to ssh-agent
+alias bashagent="ssh-agent sh -c 'ssh-add < /dev/null & bash'"
+
 # Typing 'bundle execute' is a serious PITA
 alias be='bundle exec'
 
 # Occasionally necessary to fix XCode silliness
 alias ded='rm -rf ~/Library/Developer/Xcode/DerivedData'
+
