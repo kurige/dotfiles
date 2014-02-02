55734c0e4a41544920a9912ec23b47757fbe7f0d
Christopher Gateley <christopher.gateley@gmail.com>
2014-02-02 18:56:12 -0500

    zsh: Remove hardly used env.zsh and add to export.zsh

--- system/env.zsh
+++
@@ -1,6 +0,0 @@
-#export EDITOR='subl'
-export EDITOR='vim'
-
-# Initialize RVM
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
--- system/export.zsh
+++ system/export.zsh
@@ -6,3 +6,7 @@ export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.5/jars"
 
 export PATH="./node_modules/.bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Initialize RVM
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
