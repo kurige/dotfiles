e52ab4986a048bb6c91d4e710e57ed911e02db3d
Christopher Gateley <christopher.gateley@gmail.com>
2014-02-02 18:58:22 -0500

    zsh: Add Go related exports

--- system/export.zsh
+++ system/export.zsh
@@ -7,6 +7,11 @@ export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.5/jars"
 export PATH="./node_modules/.bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
+# 'Go' setup
+export GOROOT=`brew --prefix go`/libexec
+export GOPATH=~/src/gocode
+export PATH=$PATH:$GOPATH/bin
+
 # Initialize RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
